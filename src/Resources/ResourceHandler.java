063bfa96ee131941c8319fc26d6567ec10dbd2e9
Jula <133760567+juliapazdziorek@users.noreply.github.com>
2025-07-06 21:07:26 +0100

    feat: update ResourceHandler to include additional textures and make imageMap final

--- src/Resources/ResourceHandler.java
+++ src/Resources/ResourceHandler.java
@@ -9,7 +9,7 @@ import java.awt.image.BufferedImage;
 
 public class ResourceHandler {
 
-    private Map<String, BufferedImage> imageMap;
+    private final Map<String, BufferedImage> imageMap;
 
     public ResourceHandler() {
         imageMap = new HashMap<>();
@@ -18,6 +18,14 @@ public class ResourceHandler {
 
     private void loadResources() {
         addToMap(imageMap, "src/Resources/SproutLands/water.png", "water");
+        addToMap(imageMap, "src/Resources/SproutLands/soil.png", "soil");
+        addToMap(imageMap, "src/Resources/SproutLands/grassWater.png", "grassWater");
+        addToMap(imageMap, "src/Resources/SproutLands/grass.png", "grass");
+        addToMap(imageMap, "src/Resources/SproutLands/darkGrass.png", "darkGrass");
+        addToMap(imageMap, "src/Resources/SproutLands/bridges.png", "bridges");
+        addToMap(imageMap, "src/Resources/SproutLands/waterDecor.png", "waterDecor");
+        addToMap(imageMap, "src/Resources/SproutLands/grassDecor.png", "grassDecor");
+        addToMap(imageMap, "src/Resources/SproutLands/paths.png", "paths");
     }
 
 
