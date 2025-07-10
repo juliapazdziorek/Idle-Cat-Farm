9314e5c53f5fd75e018485b8715b640a744c9107
Jula <133760567+juliapazdziorek@users.noreply.github.com>
2025-07-10 13:42:00 +0200

    feat: implement FarmCat class with movement and animation handling

--- src/Entities/Characters/FarmCat.java
+++ src/Entities/Characters/FarmCat.java
@@ -1,218 +1,264 @@
-//package Entities.Characters;
-//
-//import Entities.AnimatedEntity;
-//import Resources.Animation;
-//
-//import java.awt.*;
-//import java.util.HashMap;
-//
-//public class FarmCat extends AnimatedEntity {
-//
-//    // properties
-//    public final int catWidth = 48;
-//    public final int catHeight = 48;
-//
-//    // enums
-//    private enum FarmCatState {STANDING, WALKING, FARMING, WATERING}
-//    private enum FarmCatFacing {DOWN, UP, RIGHT, LEFT}
-//
-//    // cat state
-//    private FarmCatState farmCatState;
-//    private FarmCatFacing farmCatFacing;
-//    private boolean isMoving;
-//
-//    // animations
-//    HashMap<String, Animation> animations;
-//
-//    // constructor
-//    public FarmCat() {
-//        super(100, 100);
-//
-//        // getting animations
-//        animations = Farm.resourceHandler.getCatAnimationMap();
-//
-//        // setting cat
-//        currentAnimation = animations.get("farmCatStandingDown");
-//        farmCatFacing = FarmCatFacing.DOWN;
-//        farmCatState = FarmCatState.STANDING;
-//
-//    }
-//
-//
-//    // moving
-//    public void moveUp() {
-//        farmCatState = FarmCatState.WALKING;
-//        farmCatFacing = FarmCatFacing.UP;
-//        isMoving = true;
-//
-//    }
-//
-//    public void moveDown() {
-//        farmCatState = FarmCatState.WALKING;
-//        farmCatFacing = FarmCatFacing.DOWN;
-//        isMoving = true;
-//
-//    }
-//
-//    public void moveLeft() {
-//        farmCatState = FarmCatState.WALKING;
-//        farmCatFacing = FarmCatFacing.LEFT;
-//        isMoving = true;
-//
-//    }
-//
-//    public void moveRight() {
-//        farmCatState = FarmCatState.WALKING;
-//        farmCatFacing = FarmCatFacing.RIGHT;
-//        isMoving = true;
-//
-//    }
-//
-//
-//    // animation handling
-//    private void setAnimation() {
-//
-//        if (!isMoving) {
-//            if (farmCatState != FarmCatState.FARMING && farmCatState != FarmCatState.WATERING) {
-//                farmCatState = FarmCatState.STANDING;
-//                switch (farmCatFacing) {
-//                    case UP: {
-//                        currentAnimation = animations.get("farmCatStandingUp");
-//                        break;
-//                    }
-//                    case DOWN: {
-//                        currentAnimation = animations.get("farmCatStandingDown");
-//                        break;
-//                    }
-//                    case LEFT: {
-//                        currentAnimation = animations.get("farmCatStandingLeft");
-//                        break;
-//                    }
-//                    case RIGHT: {
-//                        currentAnimation = animations.get("farmCatStandingRight");
-//                        break;
-//                    }
-//                }
-//            }
-//
-//            /*if (farmCatState == FarmCatState.FARMING) {
-//                switch (farmCatFacing) {
-//                    case UP: {
-//                        currentAnimation = animations.get("farmCatFarmingUp");
-//                        break;
-//                    }
-//                    case DOWN: {
-//                        currentAnimation = animations.get("farmCatFarmingDown");
-//                        break;
-//                    }
-//                    case LEFT: {
-//                        currentAnimation = animations.get("farmCatFarmingLeft");
-//                        break;
-//                    }
-//                    case RIGHT: {
-//                        currentAnimation = animations.get("farmCatFarmingRight");
-//                        break;
-//                    }
-//                }
-//            }
-//
-//            if (farmCatState == FarmCatState.WATERING) {
-//                switch (farmCatFacing) {
-//                    case UP: {
-//                        currentAnimation = animations.get("farmCatWateringUp");
-//                        break;
-//                    }
-//                    case DOWN: {
-//                        currentAnimation = animations.get("farmCatWateringDown");
-//                        break;
-//                    }
-//                    case LEFT: {
-//                        currentAnimation = animations.get("farmCatWateringLeft");
-//                        break;
-//                    }
-//                    case RIGHT: {
-//                        currentAnimation = animations.get("farmCatWateringRight");
-//                        break;
-//                    }
-//                }
-//            }*/
-//
-//        } else {
-//            switch (farmCatFacing) {
-//                case UP: {
-//                    currentAnimation = animations.get("farmCatWalkingUp");
-//                    break;
-//                }
-//                case DOWN: {
-//                    currentAnimation = animations.get("farmCatWalkingDown");
-//                    break;
-//                }
-//                case LEFT: {
-//                    currentAnimation = animations.get("farmCatWalkingLeft");
-//                    break;
-//                }
-//                case RIGHT: {
-//                    currentAnimation = animations.get("farmCatWalkingRight");
-//                    break;
-//                }
-//            }
-//
-//        }
-//
-//
-//    }
-//
-//    public void resetAnimations() {
-//        animations.forEach((key, value) -> value.resetFrames());
-//    }
-//
-//
-//    // updating
-//    @Override
-//    public void update() {
-//
-//        // moving
-//        if (actionHandler.upPressed) {
-//            moveUp();
-//        }
-//        if (actionHandler.downPressed) {
-//            moveDown();
-//        }
-//        if (actionHandler.leftPressed) {
-//            moveLeft();
-//        }
-//        if (actionHandler.rightPressed) {
-//            moveRight();
-//        }
-//
-//        // set properties
-//        setAnimation();
-//        isMoving = false;
-//        currentAnimation.update();
-//
-//    }
-//
-//    // rendering
-//    @Override
-//    public void render(Graphics2D graphics2D) {
-//        graphics2D.drawImage(currentAnimation.getCurrentFrame(), positionX * scale, positionY * scale, catWidth * scale, catHeight * scale, null);
-//    }
-//
-//    public int getCatPositionX() {
-//        return positionX;
-//    }
-//
-//    public int getCatPositionY() {
-//        return positionY;
-//    }
-//
-//    public int getCenterScreenCatPositionX() {
-//        return (frame.getWidth() - catWidth * scale) / 2;
-//    }
-//
-//    public int getCenterScreenCatPositionY() {
-//        return (frame.getHeight() - catHeight * scale ) / 2;
-//    }
-//
-//
-//}
+package Entities.Characters;
+
+import Entities.AnimatedEntity;
+import Game.FocusFarm;
+import Resources.Animation;
+
+import java.awt.*;
+import java.util.HashMap;
+
+public class FarmCat extends AnimatedEntity {
+
+    // properties
+    public final int catWidth = 48;
+    public final int catHeight = 48;
+
+    // enums
+    private enum FarmCatState {STANDING, WALKING, RUNNING, TILLING, CHOPPING, WATERING}
+    private enum FarmCatFacing {DOWN, UP, RIGHT, LEFT}
+
+    // cat state
+    private FarmCatState farmCatState;
+    private FarmCatFacing farmCatFacing;
+    private Animation currentAnimation;
+    private boolean isMoving;
+
+    // animations
+    HashMap<String, Animation> animations;
+
+    // constructor
+    public FarmCat(int positionX, int positionY) {
+        super(positionX, positionY);
+
+        animations = FocusFarm.resourceHandler.createCatAnimationMap();
+
+        // setting cat state
+        currentAnimation = animations.get("farmCatStandingDown");
+        farmCatFacing = FarmCatFacing.DOWN;
+        farmCatState = FarmCatState.STANDING;
+    }
+
+
+    // moving
+    public void walkUp() {
+        farmCatState = FarmCatState.WALKING;
+        farmCatFacing = FarmCatFacing.UP;
+        isMoving = true;
+    }
+
+    public void walkDown() {
+        farmCatState = FarmCatState.WALKING;
+        farmCatFacing = FarmCatFacing.DOWN;
+        isMoving = true;
+    }
+
+    public void walkLeft() {
+        farmCatState = FarmCatState.WALKING;
+        farmCatFacing = FarmCatFacing.LEFT;
+        isMoving = true;
+    }
+
+    public void walkRight() {
+        farmCatState = FarmCatState.WALKING;
+        farmCatFacing = FarmCatFacing.RIGHT;
+        isMoving = true;
+    }
+
+    public void runUp() {
+        farmCatState = FarmCatState.RUNNING;
+        farmCatFacing = FarmCatFacing.UP;
+        isMoving = true;
+    }
+
+    public void runDown() {
+        farmCatState = FarmCatState.RUNNING;
+        farmCatFacing = FarmCatFacing.DOWN;
+        isMoving = true;
+    }
+
+    public void runLeft() {
+        farmCatState = FarmCatState.RUNNING;
+        farmCatFacing = FarmCatFacing.LEFT;
+        isMoving = true;
+    }
+
+    public void runRight() {
+        farmCatState = FarmCatState.RUNNING;
+        farmCatFacing = FarmCatFacing.RIGHT;
+        isMoving = true;
+    }
+
+
+    // animation handling
+    private void setAnimation() {
+
+        if (!isMoving) {
+            if (farmCatState != FarmCatState.TILLING && farmCatState != FarmCatState.CHOPPING && farmCatState != FarmCatState.WATERING) {
+                farmCatState = FarmCatState.STANDING;
+                switch (farmCatFacing) {
+                    case UP: {
+                        currentAnimation = animations.get("farmCatStandingUp");
+                        break;
+                    }
+                    case DOWN: {
+                        currentAnimation = animations.get("farmCatStandingDown");
+                        break;
+                    }
+                    case LEFT: {
+                        currentAnimation = animations.get("farmCatStandingLeft");
+                        break;
+                    }
+                    case RIGHT: {
+                        currentAnimation = animations.get("farmCatStandingRight");
+                        break;
+                    }
+                }
+            }
+
+            if (farmCatState == FarmCatState.TILLING) {
+                switch (farmCatFacing) {
+                    case UP: {
+                        currentAnimation = animations.get("farmCatTillingUp");
+                        break;
+                    }
+                    case DOWN: {
+                        currentAnimation = animations.get("farmCatTillingDown");
+                        break;
+                    }
+                    case LEFT: {
+                        currentAnimation = animations.get("farmCatTillingLeft");
+                        break;
+                    }
+                    case RIGHT: {
+                        currentAnimation = animations.get("farmCatTillingRight");
+                        break;
+                    }
+                }
+            }
+
+            if (farmCatState == FarmCatState.CHOPPING) {
+                switch (farmCatFacing) {
+                    case UP: {
+                        currentAnimation = animations.get("farmCatChoppingUp");
+                        break;
+                    }
+                    case DOWN: {
+                        currentAnimation = animations.get("farmCatChoppingDown");
+                        break;
+                    }
+                    case LEFT: {
+                        currentAnimation = animations.get("farmCatChoppingLeft");
+                        break;
+                    }
+                    case RIGHT: {
+                        currentAnimation = animations.get("farmCatChoppingRight");
+                        break;
+                    }
+                }
+            }
+
+            if (farmCatState == FarmCatState.WATERING) {
+                switch (farmCatFacing) {
+                    case UP: {
+                        currentAnimation = animations.get("farmCatWateringUp");
+                        break;
+                    }
+                    case DOWN: {
+                        currentAnimation = animations.get("farmCatWateringDown");
+                        break;
+                    }
+                    case LEFT: {
+                        currentAnimation = animations.get("farmCatWateringLeft");
+                        break;
+                    }
+                    case RIGHT: {
+                        currentAnimation = animations.get("farmCatWateringRight");
+                        break;
+                    }
+                }
+            }
+
+        } else {
+            if (farmCatState == FarmCatState.WALKING) {
+                switch (farmCatFacing) {
+                    case UP: {
+                        currentAnimation = animations.get("farmCatWalkingUp");
+                        break;
+                    }
+                    case DOWN: {
+                        currentAnimation = animations.get("farmCatWalkingDown");
+                        break;
+                    }
+                    case LEFT: {
+                        currentAnimation = animations.get("farmCatWalkingLeft");
+                        break;
+                    }
+                    case RIGHT: {
+                        currentAnimation = animations.get("farmCatWalkingRight");
+                        break;
+                    }
+                }
+            } else {
+                switch (farmCatFacing) {
+                    case UP: {
+                        currentAnimation = animations.get("farmCatRunningUp");
+                        break;
+                    }
+                    case DOWN: {
+                        currentAnimation = animations.get("farmCatRunningDown");
+                        break;
+                    }
+                    case LEFT: {
+                        currentAnimation = animations.get("farmCatRunningLeft");
+                        break;
+                    }
+                    case RIGHT: {
+                        currentAnimation = animations.get("farmCatRunningRight");
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    public void resetAnimations() {
+        animations.forEach((key, value) -> value.resetFrames());
+    }
+
+
+    // getters
+    public int getCatPositionX() {
+        return positionX;
+    }
+
+    public int getCatPositionY() {
+        return positionY;
+    }
+
+    public int getCenterScreenCatPositionX() {
+        return (FocusFarm.frame.getWidth() - catWidth * FocusFarm.scale) / 2;
+    }
+
+    public int getCenterScreenCatPositionY() {
+        return (FocusFarm.frame.getHeight() - catHeight * FocusFarm.scale ) / 2;
+    }
+
+
+    // updating & rendering
+    @Override
+    public void update() {
+        setAnimation();
+        isMoving = false;
+        currentAnimation.update();
+    }
+
+    @Override
+    public void render(Graphics2D graphics2D) {
+        graphics2D.drawImage(currentAnimation.getCurrentFrame(),
+                positionX * FocusFarm.scale,
+                positionY * FocusFarm.scale,
+                catWidth * FocusFarm.scale,
+                catHeight * FocusFarm.scale,
+                null);
+    }
+}
